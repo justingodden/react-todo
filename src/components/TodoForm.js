eb58c9698f1c24f9ccfc591cdfffb2513e1448d3
justingodden <justingodden@gmail.com>
2021-01-01 15:59:15 +0000

    Seperate styling for update form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 export default function TodoForm(props) {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const inputRef = useRef(null)
 
@@ -25,17 +25,38 @@ export default function TodoForm(props) {
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input
-                className="todo-input"
-                placeholder="Add a todo"
-                value={input}
-                name="text"
-                onChange={handleChange}
-                ref={inputRef}
-            />
-            <button className="todo-button" onClick={handleSubmit}>
-                Add todo
-            </button>
+            {props.edit ? (
+                <>
+                    <input
+                        className="todo-input edit"
+                        placeholder="Add a todo"
+                        value={input}
+                        name="text"
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className="todo-button edit" onClick={handleSubmit}>
+                        Update
+                    </button>
+                </>
+                    ) : (
+                <>
+                    <input
+                        className="todo-input"
+                        placeholder="Add a todo"
+                        value={input}
+                        name="text"
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className="todo-button" onClick={handleSubmit}>
+                        Add todo
+                    </button>
+                </>
+                )
+
+        }
+            
         </form>
     )
 }
