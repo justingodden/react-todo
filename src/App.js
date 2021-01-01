70667bf2ddf29debd6996d26844747c969961dd7
justingodden <justingodden@gmail.com>
2021-01-01 13:33:44 +0000

    todolist elemenform built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
 
 export default function TodoList() {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+    }
+
     return (
         <div>
-            
+            <h1>What's the plan for today?</h1>
+            <TodoForm onSubmit={addTodo}/>
         </div>
+        
     )
 }
